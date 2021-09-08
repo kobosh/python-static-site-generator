35f9e3d18d6208eb368744afd267ff325c27219f
hassan mohamed <kokobosh@gmail.com>
2021-09-08 14:09:30 -0400

    implemented Site class and ssg.py

---
+++ ssg/site.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+class Site:
+    def __init__(self,source,dest):
+        self.source=Path(source)
+        self.dest=Path(dest)
+    def create_dir(self,path):
+        directory=self.dest/relative_to(path)
+        directory.mkdir(parents=True,exist_ok=True)
+    def build(self):
+        self.dest.mkdir(parents=True,exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path is directory:
+                create_dir(path)
