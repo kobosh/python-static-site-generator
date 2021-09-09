aaf02e6a0ff375347a94454b1c1ef1ed68d40159
hassan mohamed <kokobosh@gmail.com>
2021-09-09 18:49:30 -0400

    fixed some errors in parsers ...

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,21 +1,27 @@
 from typing import List
-from pathlib import pathlib
+from pathlib import Path
+from shutil import copy2
 
 class Parser:
 
     extensions:List[str]=[]
     def valid_extension(self,extension):
-        if extension is in self.extensions:
+        if extension in self.extensions:
             return True
         return False
     def parse(self,path:Path,source:Path,dest:Path):
          raise NotImplementedError
-    def read(self path):
-        with  open(path) as f
-        bytes=f.read()
-        return bytes
+    def read(self, path):
+        with  open(path) as file:
+            bts=file.read()
+        return bts
     def write(self,path,dest,content,ext=".html"):
-        if valid_extension(ext):
+        if self.valid_extension(ext):
             extn=ext
-        destination=parse(path,source=None,dest)
-        full_path=destination/path.with_suffix(ext).name
+        destination=self.parse(path,None,dest)
+        full_path=destination/path.with_suffix(extn).name
+        with open(full_path) as file:
+            file.write(content)
+    def copy(self,path,source,dest):
+        relpath=dest/path.relative_to(source)
+        copy2(path,relpath)
--- ssg/site.py
+++ ssg/site.py
@@ -13,14 +13,17 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
-    def load_parser(extension):
+            elif path.is_file():
+                self.run_parser(path)
+    def load_parser(self,extension):
         for parser in self.parsers:
             if parser.valid_parser():
                 return parser
         return None
-    def run_parser(path):
+    def run_parser(self,path):
         parser=self.load_parser(path.suffix)
-        if parser not None:
+        if parser is not None:
             parser.parse(path,self.source,self.dest)
         else:
-            raise NotImplementedError    
+            raise NotImplementedError
+ss=Site()
