55fe8c0a281aff3d6345c82a5d925fc9971eb434
hassan mohamed <kokobosh@gmail.com>
2021-09-08 20:37:09 -0400

    corrected code until test module1 passed

--- ssg/site.py
+++ ssg/site.py
@@ -1,13 +1,14 @@
 from pathlib import Path
+import os
 class Site:
     def __init__(self,source,dest):
         self.source=Path(source)
         self.dest=Path(dest)
     def create_dir(self,path):
-        directory=self.dest/relative_to(path)
+        directory=self.dest/path.relative_to(self.source)
         directory.mkdir(parents=True,exist_ok=True)
     def build(self):
         self.dest.mkdir(parents=True,exist_ok=True)
         for path in self.source.rglob("*"):
-            if path is directory:
-                create_dir(path)
+            if path.is_dir():
+                self.create_dir(path)
