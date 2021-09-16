1d35dfa3cddffb9e4d9bd3f02e62f3355e3a0cf1
hassan mohamed <kokobosh@gmail.com>
2021-09-16 14:57:51 -0400

    edited markdown

--- ssg.py
+++ ssg.py
@@ -2,7 +2,7 @@ import typer
 from ssg.site import Site
 import ssg.parsers
 def main(source="content", dest="dist"):
-    config={"parsers":[ssg.parsers.ResourceParser()] ,   "source": source,    "dest":dest  }
+    config={"parsers":[ssg.parsers.ResourceParser(),ssg.parsers.MarkdownParser(),ssg.parsers.RestructuredTextParser()] ,   "source": source,    "dest":dest  }
     Site(**config).build()
 
 typer.run(main)
--- ssg/site.py
+++ ssg/site.py
@@ -1,3 +1,4 @@
+import sys
 from pathlib import Path
 
 class Site:
@@ -26,4 +27,8 @@ class Site:
             parser.parse(path,self.source,self.dest)
         else:
             self.error("No parser for the {} extension, file skipped!".format(path.suffix))
+
+    @staticmethod
+    def error(message):
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
 
